892e70a848511078ce62184de9855474f5430a50
yenicpanjaitan08 <nababangresya@gmail.com>
2020-06-22 14:30:36 +0700

    KP 302B

---
+++ resources/views/Home/dashboard.blade.php
@@ -0,0 +1,62 @@
+@extends('master')
+@section('content')
+
+<!-- Content Wrapper. Contains page content -->
+<section class="content">
+    <div class="container-fluid">
+        <!-- Info boxes -->
+        <div class="row">
+            <div class="col-12 col-sm-8 col-md-4">
+                <div class="info-box">
+                    <span class="info-box-icon bg-info elevation-1"><i class="fas fa-envelope"></i></span>
+
+                    <div class="info-box-content">
+                        <span class="info-box-text">Surat Masuk</span>
+                        <span class="info-box-number"> 1000
+                            <small>Terhitung </small>
+                        </span>
+                    </div>
+                    <!-- /.info-box-content -->
+                </div>
+                <!-- /.info-box -->
+            </div>
+            <!-- /.col -->
+            <div class="col-12 col-sm-8 col-md-4">
+                <div class="info-box mb-3">
+                    <span class="info-box-icon bg-danger elevation-1"><i class="fas fa-envelope-open"></i></span>
+
+                    <div class="info-box-content">
+                        <span class="info-box-text">Surat Keluar</span>
+                        <span class="info-box-number">1000
+                            <small>Terhitung</small>
+                        </span>
+                    </div>
+                    <!-- /.info-box-content -->
+                </div>
+                <!-- /.info-box -->
+            </div>
+            <!-- /.col -->
+            <div class="col-12 col-sm-8 col-md-4">
+                <div class="info-box mb-3">
+                    <span class="info-box-icon bg-danger elevation-1"><i class="fas fa-share-square"></i></span>
+
+                    <div class="info-box-content">
+                        <span class="info-box-text">Disposisi</span>
+                        <span class="info-box-number">1000
+                            <small>Terhitung</small>
+                        </span>
+                    </div>
+                    <!-- /.info-box-content -->
+                </div>
+                <!-- /.info-box -->
+            </div>
+            <div class="panel">
+               
+            </div>
+        </div>
+
+        <!-- /.row -->
+</section>
+
+
+@endsection
